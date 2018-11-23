32b46d110b03854d5326b5105059562923669c96
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2018-11-23 17:23:23 +0800

    Update build since I can't deploy to DO properly

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bf2c4f285a51e6dc29baa748abb99c2.js"
+  "/precache-manifest.a452fd1fa0a87050f2802f6f9fd5bb86.js"
 );
 
 workbox.clientsClaim();
