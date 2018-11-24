1e918f88203fdb9fe3dd656fbabde799c70d44d3
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2018-11-24 16:22:17 +0800

    Add new build

---
+++ frontend/build/precache-manifest.1b7a92b8e40d72dd13b723acb76c6144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "262af0af7c4ab6cf6156",
+    "url": "/static/js/main.262af0af.chunk.js"
+  },
+  {
+    "revision": "c5daa7be51da5bc2fcc4",
+    "url": "/static/js/1.c5daa7be.chunk.js"
+  },
+  {
+    "revision": "262af0af7c4ab6cf6156",
+    "url": "/static/css/main.c4ce4a7a.chunk.css"
+  },
+  {
+    "revision": "c5daa7be51da5bc2fcc4",
+    "url": "/static/css/1.7bf8c1ab.chunk.css"
+  },
+  {
+    "revision": "a6e75ae3a3226d4c3fabe2e5a1ed0d3d",
+    "url": "/index.html"
+  }
+];
